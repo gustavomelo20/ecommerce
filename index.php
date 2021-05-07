df1cbc2d4ba88583f727bffd2523a708801882a0
gustavomelo20 <infogustavomelo@gmail.com>
2021-05-07 19:41:35 -0300

    router admin

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php 
-
+ 
 require_once("vendor/autoload.php");
-
-use \Slim\Slim;
-use  \Hcode\page;
-
+ 
+use  \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+ 
 $app = new Slim();
-
+ 
 $app->config('debug', true);
-
+ 
 $app->get('/', function() {
     
 	$page = new Page();
-	$page->setTpl("index");
-
+ 
+	$page ->setTpl("index");
+ 
 });
-
+ 
+ 
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+ 
+	$page ->setTpl("index");
+ 
+});
+ 
 $app->run();
-
- ?>
+ 
+?>
